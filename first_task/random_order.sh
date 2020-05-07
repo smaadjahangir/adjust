f29ff57172f1b5fd7aad64838c0c8efa727af907
S. Maad Jahangir <smaadjahangir@gmail.com>
2020-05-07 16:52:15 +0200

    random_order.sh change

--- first_task/random_order.sh
+++ first_task/random_order.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # Variable as Input
-START_INPUT=$1
-END_INPUT=$2
-START=${START_INPUT:-1}
-END=${END_INPUT:-10}
+START=${1:-1}
+END=${2:-10}
 
 # If variable has no Input then taking default value
-if [ -z "$START_INPUT" ] || [ -z "$END_INPUT" ]
+if [ -z "$START" ] || [ -z "$END" ]
 then
       echo "Msg: Start or End number missing, taking default value for missing number"
 fi
